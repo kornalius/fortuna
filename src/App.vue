3f60a68095a95ddae65798ef3d29d73eb71b3add
kornalius <dehaen@bewave.io>
2022-02-04 11:05:10 -0500

    Working App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <n-config-provider :theme="darkTheme">
     <Main v-if="game.isStarted" />
-    <n-dialog
-      :value="showDialog"
+    <n-modal
+      :show="showDialog"
+      style="width: 600px;"
+      role="dialog"
+      aria-modal="true"
     >
       <Menu />
-    </n-dialog>
+    </n-modal>
   </n-config-provider>
 </template>
 
@@ -16,9 +19,9 @@ import Menu from '@/components/Menu.vue'
 import { store } from '@/store'
 import { darkTheme } from 'naive-ui'
 
-const game = computed(() => store.game)
+const { game } = store
 
-const showDialog = computed(() => game.isPaused || !game.isStarted)
+const showDialog = computed(() => game.isPaused || game.isStarted === false)
 </script>
 
 <style>
