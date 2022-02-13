b3c0be84e7bbf46026844eea88e58df8da773eeb
kornalius <dehaen@bewave.io>
2022-02-13 09:49:00 -0500

    New words

--- src/words.js
+++ src/words.js
@@ -1,18 +1,24 @@
-export const filetypes = ['str', 'dat', 'fnt', 'vid', 'htp', 'msh', 'frm', 'cmd', 'crd', 'aud', 'doc', 'cod', 'pre',
-  'key', 'lst']
+export const filetypes = [
+  'str', 'dat', 'fnt', 'vid', 'htp', 'msh', 'frm', 'cmd', 'crd', 'aud', 'doc', 'cod', 'pre', 'key', 'lst',
+]
 
-export const adjectives = ['big', 'small', 'huge', 'tiny', 'large', 'bulky', 'dummy', 'stupid', 'important', 'urgent',
+export const adjectives = [
+  'big', 'small', 'huge', 'tiny', 'large', 'bulky', 'dummy', 'stupid', 'important', 'urgent',
   'secret', 'advanced', 'great', 'first', 'last', 'prime', 'clean', 'cute', 'fantastic', 'funny', 'good', 'glorious',
   'helpful', 'fine', 'helpful', 'modern', 'old', 'odd', 'open', 'close', 'perfect', 'real', 'silly', 'super', 'stupid',
   'tasty', 'vast', 'tough', 'wrong', 'basic', 'expanded', 'clear', 'complete', 'custom', 'definite', 'easy', 'familiar',
   'explosive', 'final', 'formal', 'fresh', 'legal', 'local', 'global', 'mark', 'minimal', 'original', 'optional',
-  'private', 'regular', 'single', 'threat', 'wrong']
+  'private', 'regular', 'single', 'threat', 'wrong',
+]
 
-export const fileNouns = ['names', 'list', 'report', 'mail', 'phones', 'image', 'bank', 'file', 'development', 'equity',
+export const fileNouns = [
+  'names', 'list', 'report', 'mail', 'phones', 'image', 'bank', 'file', 'development', 'equity',
   'holdings', 'trust', 'summit', 'expenses', 'business', 'trip', 'research', 'advertisement', 'paint', 'insurance',
-  'camera', 'law', 'library', 'medical', 'magazine', 'market', 'engine', 'note', 'book']
+  'camera', 'law', 'library', 'medical', 'magazine', 'market', 'engine', 'note', 'book',
+]
 
-export const femaleNames = ['annie', 'amy', 'angel', 'asia', 'audrey', 'bella', 'bess', 'beverly', 'brook', 'cailyn',
+export const femaleNames = [
+  'annie', 'amy', 'angel', 'asia', 'audrey', 'bella', 'bess', 'beverly', 'brook', 'cailyn',
   'caren', 'carla', 'carolyn', 'casie', 'cathy', 'clara', 'corie', 'daisy', 'dalia', 'dana', 'dan', 'daniele', 'debbie',
   'dee', 'denise', 'eden', 'edna', 'eliza', 'elois', 'elyse', 'erma', 'eryn', 'eva', 'eve', 'farah', 'faith', 'flora',
   'gena', 'diana', 'giana', 'gina', 'grace', 'hanna', 'hayden', 'helen', 'hope', 'hilma', 'ilma', 'isabel', 'ivonne',
@@ -21,9 +27,11 @@ export const femaleNames = ['annie', 'amy', 'angel', 'asia', 'audrey', 'bella', 
   'mandy', 'marcy', 'mary', 'mily', 'mia', 'nancy', 'nelly', 'nora', 'olga', 'opal', 'paige', 'pam', 'paris', 'patty',
   'peggy', 'rachel', 'rebeca', 'rene', 'riley', 'rose', 'roxy', 'ruth', 'salma', 'sandy', 'sarah', 'selma', 'sabrina',
   'sandra', 'sharon', 'sharon', 'simone', 'sonia', 'sophie', 'stacy', 'sue', 'talia', 'tania', 'teresa', 'tina', 'tony',
-  'tonia', 'tracy', 'trish', 'val', 'vera', 'wanda', 'wendy', 'wilma']
+  'tonia', 'tracy', 'trish', 'val', 'vera', 'wanda', 'wendy', 'wilma',
+]
 
-export const maleNames = ['aaron', 'abe', 'adam', 'adrien', 'alan', 'alec', 'alex', 'alfred', 'andre', 'archie', 'arnold',
+export const maleNames = [
+  'aaron', 'abe', 'adam', 'adrien', 'alan', 'alec', 'alex', 'alfred', 'andre', 'archie', 'arnold',
   'barry', 'beau', 'ben', 'bernie', 'bill', 'blake', 'bob', 'bobby', 'brady', 'brendan', 'brad', 'brant', 'brent', 'bryan',
   'bret', 'brock', 'brody', 'bruno', 'bud', 'bruce', 'burt', 'carey', 'carlos', 'cary', 'cesar', 'charles', 'chuck', 'clark',
   'clay', 'cletus', 'cliff', 'clive', 'clint', 'cole', 'cody', 'colin', 'cooper', 'cory', 'chris', 'dale', 'dan', 'daniel',
@@ -45,11 +53,149 @@ export const maleNames = ['aaron', 'abe', 'adam', 'adrien', 'alan', 'alec', 'ale
   'roman', 'ronald', 'ross', 'roy', 'rowan', 'roswell', 'rubin', 'ruby', 'rudy', 'rufus', 'russ', 'sam', 'samuel', 'sammy',
   'saul', 'scott', 'sean', 'seth', 'seymour', 'sid', 'simon', 'spencer', 'stan', 'stanley', 'steve', 'steven', 'stevie',
   'stewart', 'ted', 'teddy', 'theo', 'tim', 'timmy', 'tobe', 'tobin', 'todd', 'tom', 'tommy', 'tony', 'troy', 'trey', 'tucker',
-  'turner', 'tyron', 'vick', 'vincent', 'vince', 'walt', 'wayne', 'warren', 'wesley', 'william', 'willy', 'will', 'zach']
+  'turner', 'tyron', 'vick', 'vincent', 'vince', 'walt', 'wayne', 'warren', 'wesley', 'william', 'willy', 'will', 'zach'
+]
 
-export const lastNames = ['abbott', 'adams', 'armstrong', 'barrows', 'bogan', 'boyle', 'brown', 'carter', 'casper', 'cole',
+export const lastNames = [
+  'abbott', 'adams', 'armstrong', 'barrows', 'bogan', 'boyle', 'brown', 'carter', 'casper', 'cole',
   'collins', 'corwin', 'davis', 'crooks', 'dickinson', 'douglas', 'doyle', 'ferry', 'fisher', 'gibson', 'glover', 'goodwin',
   'graham', 'green', 'haley', 'hamill', 'hane', 'hayes', 'herman', 'hills', 'howell', 'hudson', 'jones', 'johns', 'jenkins',
   'king', 'larson', 'larkin', 'lindgren', 'little', 'lynch', 'marks', 'mills', 'miller', 'moore', 'muller', 'nolan',
   'parker', 'price', 'ritchie', 'reynolds', 'reilly', 'roberts', 'sanford', 'schmitt', 'smith', 'shields', 'stark',
-  'swift', 'upton', 'von', 'walker', 'weber', 'welch', 'williams', 'ziemann']
+  'swift', 'upton', 'von', 'walker', 'weber', 'welch', 'williams', 'ziemann',
+]
+
+export const bigAdjectives = [
+  'big', 'colossal', 'fat', 'gigantic', 'great', 'huge', 'immense', 'large', 'mammoth', 'massive', 'tall',
+]
+
+export const smallAdjectives = [
+  'little', 'miniature', 'petite', 'puny', 'scrawny', 'short', 'small', 'teeny', 'teeny-tiny', 'tiny',
+]
+
+export const colorAdjectives = [
+  'light', 'pale', 'dark', 'deep',
+]
+
+export const adverbs = [
+  'very', 'extremely', 'exceedingly', 'exceptionally', 'extraordinarily', 'tremendously', 'immensely', 'intensely',
+  'abundantly', 'uncommonly', 'particularly', 'highly', 'remarkably', 'really', 'truly',
+]
+
+export const hairColors = [
+  'black', 'white', 'brown', 'grey', 'auburn', 'blond', 'chestnut',
+]
+
+export const eyeColors = [
+  'black', 'brown', 'grey', 'blue', 'green', 'hazel',
+]
+
+export const skinColors = [
+  'black', 'brown', 'white', 'yellow',
+]
+
+export const smallSizes = [
+  'skinny', 'lean', 'weak', 'little', 'miniature', 'tiny',
+]
+
+export const normalSizes = [
+  'average', 'typical', 'standard', 'regular', 'usual',
+]
+
+export const largeSizes = [
+  'fat', 'obese', 'brawny', 'muscular', 'big', 'colossal', 'huge', 'massive',
+]
+
+export const short = [
+  'small', 'little', 'short',
+]
+
+export const tall = [
+  'tall', 'high', 'lofty', 'eminent', 'prominent',
+]
+
+export const poor = [
+  'poor', 'needy', 'penniless', 'broke', 'insolvent',
+]
+
+export const adequate = [
+  'acceptable', 'capable', 'adequate', 'fair', 'decent', 'satisfactory', 'sufficient', 'comfortable', 'passable',
+]
+
+export const rich = [
+  'upscale', 'wealthy', 'well-off', 'loaded', 'opulent', 'affluent',
+]
+
+export const socialInterests = [
+  'flirting', 'networking', 'people', 'helping', 'listening', 'talking',
+]
+
+export const politicalInterests = [
+  'government', 'freedom', 'truth', 'leading',
+]
+
+export const workInterests = [
+  'crafting', 'exploring', 'working', 'building', 'learning', 'exercise',
+]
+
+export const hobbyInterests = [
+  'nature', 'cities', 'animals', 'food', 'sunshine',
+]
+
+export const badInterests = [
+  'fighting', 'lying', 'drinking', 'stealing', 'vandalism', 'storms', 'slacking',
+]
+
+export const happyMoods = [
+  'cheerful', 'content', 'ecstatic', 'happy', 'joyful', 'jubilant', 'pleased', 'gentle', 'good', 'peaceful',
+  'sympathetic', 'thoughtful',
+]
+
+export const sadMoods = [
+  'annoyed', 'apathetic', 'ashamed', 'bittersweet', 'bored', 'cynical', 'depressed', 'discontent', 'pessimistic',
+  'sad',
+]
+
+export const angryMoods = [
+  'aggravated', 'angry', 'cranky', 'enraged', 'frustrated', 'grumpy', 'infuriated', 'irritated', 'mad',
+]
+
+export const energeticMoods = [
+  'energetic', 'excited', 'high', 'hyper', 'intense', 'lively',
+]
+
+export const tiredMoods = [
+  'drained', 'exhausted', 'lazy', 'lethargic', 'morose', 'sleepy',
+]
+
+export const dangerousMoods = [
+  'chaotic', 'crazy', 'demented', 'predatory', 'psychotic',
+]
+
+export const calmMoods = [
+  'calm', 'chilled', 'relaxed',
+]
+
+export const stressedMoods = [
+  'anxious', 'cautious', 'defensive', 'paranoid', 'scared', 'stressed', 'shocked',
+]
+
+export const stateMoods = [
+  'cold', 'confused', 'curious', 'delusional', 'dorky', 'empathetic', 'envious', 'fearful', 'impatient', 'indifferent',
+  'melancholic', 'mellow', 'passionate', 'rebellious', 'rejected', 'strange', 'weird',
+]
+
+export const intelligence = [
+  'smart', 'dumb', 'stupid', 'intelligent', 'average', 'slow', 'fast', 'bright', 'sharp', 'intellectual', 'clever', 'ignorant',
+  'simple', 'idiot', 'thick',
+]
+
+export const motives = [
+  'money', 'love', 'fate', 'sickness', 'glory', 'revenge',
+]
+
+export const traits = [
+  'curious', 'studious', 'mysterious', 'rough', 'silent', 'skeptic', 'immature', 'materialistic', 'dreamy',
+  'creative', 'romantic', 'frivolous', 'aloof', 'introvert', 'extrovert', 'leader', 'follower',
+  'emotional', 'emotionless', 'humorous', 'grim', 'selfsufficent', 'dependent', 'logical', 'illogical',
+]
